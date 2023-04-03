6da66fc0af6f9d14df5fa39e7efe22ed59be6c89
Ubuntu <ubuntu@ip-172-31-93-18.ec2.internal>
2023-04-03 03:24:06 +0000

    first

---
+++ chatbot_terminal.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+def ask_chatgpt():
+    import openai
+    openai.api_key = "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"
+    new_input='User: Hello!'
+    historical_infos = ''
+    prompt = historical_infos + '\n' + new_input + '\n'
+    
+    while True:
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            max_tokens=2000,
+            temperature=0.7,
+            messages=[{'role':'user','content':prompt}]
+            
+        )
+
+        response = res.to_dict()['choices'][0]['message']['content']
+        print(response)
+        print('===============','\n\n\n')
+        
+        ### store historical infos
+        historical_infos = historical_infos + '\n' + "User: " + new_input + '\n' + 'AI: ' + response + '\n'
+        
+        ### make new input
+        print('===============')
+        new_input = input("User: ")
+        prompt = historical_infos + '\n' + "User: " + new_input + '\n'
+        
+        if prompt.lower() == "user: quit":
+            break
+
+
+if __name__=='__main__':
+    ask_chatgpt()
+
+
+
+
